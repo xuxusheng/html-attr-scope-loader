0bff1aeba75afaf79a2c15144a5bb6d6ee1a734c
xusheng-antiy <20691718@qq.com>
2016-08-19 16:12:55 +0800

    1. 替换 img 标签 src 属性的方式改了一下 2. 更新版本号到 1.2.2

--- index.js
+++ index.js
@@ -26,15 +26,21 @@ module.exports = function (content) {
     // links 中存放待引入的 css 文件的相对路径
     var links = parseResults.links
 
+    // imgSrcMap 中存放 img 标签中 src 属性的 键值对， { 'xxxHTMLLINKxxx12345678xxx': './avatar.png' }
+    var imgSrcMap = parseResults.imgSrcMap
+
     // newHtml 中存放最后 callback 中需要抛出的 html 字符串
-    var newHtml = JSON.stringify(parseResults.newHtml).replace(/&quot;/g, '"')
+    var newHtml = JSON.stringify(parseResults.newHtml)
 
     var results = []
 
     if (links.length !== 0) results = processLinks(links, scope)
 
     results.push(
-        'module.exports = ' + newHtml + ';'
+        'module.exports = ' + newHtml.replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, function (match) {
+            if (!imgSrcMap[match]) return match
+            return '" + require(' + JSON.stringify(loaderUtils.urlToRequest(imgSrcMap[match])) + ') + "';
+        }) + ';'
     )
 
     callback(null, results.join('\r\n'))
